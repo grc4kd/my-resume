f7da813d6141826f6635b679d746fa27db582ee8
grc4kd <grc4kd@outlook.com>
2024-04-30 22:08:59 -0500

    Fixed ExperienceArticleComponent unit test by mocking data input for expected experience. Tested for prescence of positive style value applied from style sheet.

--- my-resume-app/src/app/experience-article/experience-article.component.spec.ts
+++ my-resume-app/src/app/experience-article/experience-article.component.spec.ts
@@ -14,6 +14,16 @@ describe('ExperienceArticleComponent', () => {
     
     fixture = TestBed.createComponent(ExperienceArticleComponent);
     component = fixture.componentInstance;
+    // mock data for experience passed in from parent component
+    const expectedExperience = {
+      title: "Software Developer",
+      description: `A person who designs and creates software to produce a desired output by composing programs from 
+      existing libraries and programming languages, often resulting in the creation of new computer source code.`,
+      company: "Mastco",
+      startDate: new Date(2001, 0, 1),
+      endDate: new Date(2001, 11, 31)
+    };
+    component.experience = expectedExperience;
     fixture.detectChanges();
   });
 
@@ -23,9 +33,10 @@ describe('ExperienceArticleComponent', () => {
 
   it('should apply expected styles to experience dates', () => {
     const experienceArticleEl: HTMLElement = fixture.nativeElement;
-    const dateDiv = experienceArticleEl.querySelector('div[class="dates"]');
-    
-    expect(dateDiv !== null);
-    expect(dateDiv?.computedStyleMap().get('padding') != "");
+    const dateDiv = experienceArticleEl.querySelector('div[class="dates"]');    
+    const styleMap = dateDiv?.computedStyleMap();
+    const paddingTop = styleMap?.get('padding-top');
+
+    expect((paddingTop as CSSUnitValue).value).toBeGreaterThan(0);
   });
 });
