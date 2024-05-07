3f633fbb61a9528186bdc2085e60ea221127e0a8
grc4kd <grc4kd@outlook.com>
2024-05-07 14:39:51 -0500

    Wait for timeout in test setup for Firestore using sample code as a guideline and an async function.
    beforeEach and testing functions are still synchronous, but the tests use the jasmine DoneFn to indicate when calls to asynchronous methods are completed.

--- my-resume-app/src/app/services/firebase-app.service.spec.ts
+++ my-resume-app/src/app/services/firebase-app.service.spec.ts
@@ -1,6 +1,5 @@
 import { FirebaseAppService } from './firebase-app.service';
 import { WebLink } from '../../data/webLink';
-import { from } from 'rxjs';
 import { EXPERIENCES } from '../../data/mock-experiences';
 import { Firestore, connectFirestoreEmulator, doc, getFirestore, setDoc, writeBatch } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
@@ -9,45 +8,42 @@ import { TestBed } from '@angular/core/testing';
 import { DatePipe } from '@angular/common';
 
 describe('FirebaseAppService', () => {
-  const datePipe = new DatePipe('en-US', undefined, {dateFormat: 'y-MM-dd'});
+  const datePipe = new DatePipe('en-US', undefined, { dateFormat: 'y-MM-dd' });
   const expectedLink: WebLink = { url: 'https://github.com' };
 
   let firebaseAppService: FirebaseAppService;
-  
-  beforeAll(() => {
+
+  beforeAll(async () => {
     const db = getFirestore(initializeApp(firebaseConfig));
 
     /* 
      * To avoid tests against a cloud-hosted instance, an emulator is utilized on the same machine 
      * running tests. 
      * For more information see: Connect your app to the Cloud Firestore Emulator:
-     * https://firebase.google.com/docs/emulator-suite/connect_firestore */ 
+     * https://firebase.google.com/docs/emulator-suite/connect_firestore */
     connectFirestoreEmulator(db, '127.0.0.1', 8080);
 
     // set test documents in emulator database
-    setDoc(doc(db, 'web-links', 'github'), expectedLink);
-    
+    await setDoc(doc(db, 'web-links', 'github'), expectedLink);
+
     const batch = writeBatch(db);
     EXPERIENCES.forEach(experience => {
       const whitespaceRegex = /\s/gi;
       let experienceTitleFormat = experience.title.replaceAll(whitespaceRegex, '-').toLowerCase();
       let startDateFormat = datePipe.transform(experience.startDate.toDate());
       let endDateFormat = datePipe.transform(experience.endDate.toDate());
-  
-      batch.set(doc(db, "experiences", 
-      `mock-${experienceTitleFormat}-${startDateFormat}-${endDateFormat}`), experience);
+
+      batch.set(doc(db, "experiences",
+        `mock-${experienceTitleFormat}-${startDateFormat}-${endDateFormat}`), experience);
     });
-  
-    from(batch.commit()).subscribe({
-      next: () => console.log("Test data load complete."),
-      error: (err) => console.error(`Test data load failed, reason: ${err}`)
-    });
+
+    await batch.commit();
   });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        {provide: Firestore, useValue: getFirestore(initializeApp(firebaseConfig))}
+        { provide: Firestore, useValue: getFirestore(initializeApp(firebaseConfig)) }
       ]
     });
 
@@ -61,12 +57,12 @@ describe('FirebaseAppService', () => {
         done();
       },
       error: (err) => fail(err)
-    }); 
+    });
   });
 
   it('should return data for work experiences', (done: DoneFn) => {
     const expectedExperiences = EXPERIENCES;
-    
+
     firebaseAppService.WorkExperiences.subscribe({
       next: (experiences) => {
         expect(experiences.length).toBeGreaterThan(0);
@@ -76,7 +72,7 @@ describe('FirebaseAppService', () => {
           const expectedEndTime = experience.endDate.toMillis();
           const expected = expectedExperiences.find(e => e.title === experience.title && e.startDate.toMillis() === expectedStartTime && e.endDate.toMillis() === expectedEndTime);
           expect(expected).toBeDefined();
-          
+
           // narrow type after test for defined
           if (expected) {
             expect(experience).toEqual(expected);
