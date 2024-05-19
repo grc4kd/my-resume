5bb6e15902d0f73d85b710553e5e3717d462b71f
grc4kd <griffin.crane@outlook.com>
2024-05-19 14:45:29 -0500

    Remove unused imports.

--- my-resume-app/src/app/app.component.spec.ts
+++ my-resume-app/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FirebaseAppService } from './services/firebase-app.service';
 import { EXPERIENCES } from '../data/mock-experiences';
