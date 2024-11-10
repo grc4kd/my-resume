1d3d1b10c60c5a1b42455aed9ccebb9a333ea833
Griffin Crane <grc4kd@outlook.com>
2024-11-10 00:30:55 -0600

    Enable App Check / recaptcha for Firebase.

--- my-resume-app/src/app/app.component.ts
+++ my-resume-app/src/app/app.component.ts
@@ -3,13 +3,25 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { Firestore, getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 import { ExperiencesComponent } from "./experiences/experiences.component";
 import { FirebaseAppService } from './services/firebase-app.service';
 import { SvgIconService } from './services/svg-icon.service';
 import { firebaseConfig } from '../../secrets/firebase-config';
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LcxNXoqAAAAABpeivcJ-PyK6bOQTqL0RdpXq0Jc'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 const db = getFirestore();
 
 @Component({
@@ -28,7 +40,7 @@ const db = getFirestore();
 })
 export class AppComponent implements OnInit {
   title = 'my-resume-app';
-  gitHubUrl: string = '';
+  gitHubUrl = '';
 
   private firebaseAppService = inject(FirebaseAppService);
 
