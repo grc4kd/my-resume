ad06d96522f0b00fe7c27ca29b4dde1169e408c3
Griffin <166270386+grc4kd@users.noreply.github.com>
2024-11-13 23:33:38 -0600

    Merge from prior work to bring generated debug token in to test App Check. (#9)

--- my-resume-app/src/app/app.component.ts
+++ my-resume-app/src/app/app.component.ts
@@ -15,41 +15,7 @@ import { environment } from '../environments/environment';
 
 const app = initializeApp(firebaseConfig);
 
-// Use debug tokens for App Check in debug builds for localhost and CI
-if (environment.useAppCheck) {
-  // when true, debug token env configuration will emit a debug token in the JavaScript console
-  // which can then be set up in Firebase - App Check console
-  let appCheckDebugToken: boolean | string = environment.useAppCheckDebugToken;
-
-  // CI environments require generated token from Firebase - App Check console
-  if (environment.useCIAppCheckDebugToken) {
-    const appCheckDebugTokenFromCI = Object.values(globalThis).some(gb => gb === 'APP_CHECK_DEBUG_TOKEN_FROM_CI');
-    if (typeof appCheckDebugTokenFromCI === "string") {
-      appCheckDebugToken = appCheckDebugTokenFromCI;
-    }
-  }
-
-  // The globalThis global property allows one to access the global object regardless of the current environment.
-  Object.defineProperty(globalThis, 
-    'FIREBASE_APPCHECK_DEBUG_TOKEN', {
-    value: appCheckDebugToken,
-    enumerable: false,
-    configurable: true,
-    writable: true,
-  });
-}
-
-if (environment.useAppCheck) {
-  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-  // key is the counterpart to the secret key you set in the Firebase console.
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(appCheckSiteKey),
-  
-    // Optional argument. If true, the SDK automatically refreshes App Check
-    // tokens as needed.
-    isTokenAutoRefreshEnabled: true
-  });
-}
+setupAppCheck();
 
 const db = getFirestore();
 
@@ -83,3 +49,58 @@ export class AppComponent implements OnInit {
     });
   }
 }
+
+function setupAppCheck() {
+  if (!environment.useAppCheck && environment.production) {
+    console.warn(`Firebase App "${app.name}": App Check has been disabled. Please turn it back on.`);
+  }
+
+  // Use debug tokens for App Check in debug builds for localhost and CI
+  if (environment.useAppCheck) {
+    // when true, debug token env configuration will emit a debug token in the JavaScript console
+    // which can then be set up in Firebase - App Check console
+    let appCheckDebugToken: boolean | string = environment.useAppCheckDebugToken;
+
+    // CI environments require generated token from Firebase - App Check console
+    if (environment.useCIAppCheckDebugToken) {
+      // load App Check CI debug token from the CI Node.js environment's global variables
+      if (Object.keys(globalThis).includes("APP_CHECK_DEBUG_TOKEN_FROM_CI")) {
+        const appCheckDebugTokenFromCI = Object.values(globalThis).find(gb => gb === "APP_CHECK_DEBUG_TOKEN_FROM_CI");
+
+        // App Check debug token should be a GUID, but just check that it isn't the empty string
+        if (typeof appCheckDebugTokenFromCI === 'string' &&
+          appCheckDebugTokenFromCI.length === 0) {
+          appCheckDebugToken = Object.values(globalThis).find(
+            (gb) => gb === 'APP_CHECK_DEBUG_TOKEN_FROM_CI'
+          );
+          console.info('The App Check debug token was loaded from the CI environment.');
+        }
+        else {
+          console.error('The debug token could not be loaded from the CI environment.');
+        }
+      }
+    }
+
+    // The globalThis global property allows one to access the global object regardless of the current environment.
+    Object.defineProperty(globalThis,
+      'FIREBASE_APPCHECK_DEBUG_TOKEN', {
+      value: appCheckDebugToken,
+      enumerable: false,
+      configurable: true,
+      writable: true,
+    });
+  }
+
+  if (environment.useAppCheck) {
+    // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+    // key is the counterpart to the secret key you set in the Firebase console.
+    initializeAppCheck(app, {
+      provider: new ReCaptchaV3Provider(appCheckSiteKey),
+
+      // Optional argument. If true, the SDK automatically refreshes App Check
+      // tokens as needed.
+      isTokenAutoRefreshEnabled: true
+    });
+  }
+}
+
