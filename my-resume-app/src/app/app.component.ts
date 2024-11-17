3a0d8f17639407f8965dedf40d422e982acf57e5
Griffin <166270386+grc4kd@users.noreply.github.com>
2024-11-17 17:22:51 -0600

    Add a loading indicator using the buffer progress bar from Angular Material (#18)
    * Add a loading indicator using the buffer progress bar from Angular Material.
    
    * Updated reference to moved export `app-check/setupAppCheck.ts`.

--- my-resume-app/src/app/app.component.ts
+++ my-resume-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+
 import { Firestore, getFirestore } from 'firebase/firestore';
 
 import { initializeApp } from 'firebase/app';
@@ -11,23 +12,26 @@ import { SvgIconService } from './services/svg-icon.service';
 import { firebaseConfig } from '../../secrets/firebase-config';
 import { environment } from '../environments/environment';
 import { setupEmulator } from './services/helpers/setupEmulator';
-import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
-import { appCheckSiteKey } from '../../secrets/app-check-config';
+import { setupAppCheck } from '../../app-check/setupAppCheck';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-const app = initializeApp(firebaseConfig);
+/**
+ * Object literal type alias containing data about a special reserved environment variable for App Check.
+ * @var name The name of the debug token environment variable from documentation.
+ * @var host The online documentation host server FQDN.
+ * @var filename The document from the documentation server.
+ * @var version (as of 2024-11-12) A publishing tag containing a date, revision, and release candidate number
+ */
+export const AppCheckDebugToken = {
+  name: 'FIREBASE_APPCHECK_DEBUG_TOKEN',
+  host: 'firebase.google.com',
+  filename: '/docs/app-check/web/debug-provider',
+  version: 't-devsite-webserver-20241112-r02-rc01.464830024708363737',
+};
 
-if (environment.useAppCheck || environment.production) {
-  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-  // key is the counterpart to the secret key you set in the Firebase console.
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(appCheckSiteKey),
+export const app = initializeApp(firebaseConfig);
 
-    // Optional argument. If true, the SDK automatically refreshes App Check
-    // tokens as needed.
-    isTokenAutoRefreshEnabled: true,
-  });
-}
-
+setupAppCheck();
 
 const db: Firestore = getFirestore();
 
@@ -40,11 +44,12 @@ if (environment.useFirebaseEmulator) {
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [MatToolbarModule, MatIconModule, ExperiencesComponent],
+  imports: [MatToolbarModule, MatIconModule, ExperiencesComponent, MatProgressBarModule],
 })
 export class AppComponent implements OnInit {
   title = 'my-resume-app';
   gitHubUrl = '';
+  nowLoading: boolean = true;
 
   constructor(private firebaseAppService: FirebaseAppService) {
     inject(SvgIconService);
@@ -53,6 +58,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.firebaseAppService.getGitHubLink().subscribe((webLink) => {
       this.gitHubUrl = webLink.url;
+
+      this.nowLoading = false;
     });
   }
 }
